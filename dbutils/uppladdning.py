48233d909047cdcd91b537425a23f4e8ba12cb17
Otto Jarhult <otto.jarhult@ad.evidensgruppen.se>
2023-01-17 10:01:41 +0100

    Fungerar ej

--- dbutils/uppladdning.py
+++ dbutils/uppladdning.py
@@ -9,7 +9,7 @@ def ladda_upp(
     tabell  : str, 
     databas : str,
     server  : str='mysql',
-    schema  : Union[dict, None]=None
+    dtypes  : Union[dict, None]=None
 ) -> None:
     ''' Laddar upp df till vald databas. Uppladdning bygger i hög grad på pandas to_sql()-funktion. Vissa tillägg har
         gjorts för att passa våra behov, bl.a. laddas data inte direkt upp till tabellen "tabell" utan först till 
@@ -20,7 +20,7 @@ def ladda_upp(
             tabell  : Namn på tabell som ska genereras 
             databas : Namn på databas/schema
             server  : Den server på vilken databasen ligger/ska ligga
-            schema  : Datatyper för kolumnerna i df
+            dtypes  : Datatyper för kolumnerna i df
     '''
 
     # Tillåtna servrar och korresponderande anslutningsfunktioner
@@ -46,15 +46,35 @@ def ladda_upp(
 
     print(f'\nLaddar upp data till "{tmp_tabell}"')
 
-    # Ladda upp data till ny tabell med suffix "_tmp"
-    if schema is None:
-        # Om inga datatyper specificerats väljs dessa automatiskt av pandas
-        df.to_sql(tmp_tabell, con=con, if_exists='replace', chunksize=10000, index_label='id')
+    sql_params = {
+        'name'        : tmp_tabell,
+        'con'         : con,
+        'if_exists'   : 'replace', 
+        'chunksize'   : 10000, 
+        'index_label' : 'id'
+    }
 
-    else:
+    if server=='postgres':
+        sql_params['schema']='public'
+
+
+
+    # Ladda upp data till ny tabell med suffix "_tmp"
+    # if schema is None:
+        # Om inga datatyper specificerats väljs dessa automatiskt av pandas
+        # df.to_sql(tmp_tabell, con=con, if_exists='replace', chunksize=10000, index_label='id')
+
+    # else:
         # Om datatyper specificerats används keys för att se till att kolumnerna i df kommer i rätt ordning
-        df = df[list(schema.keys())]
-        df.to_sql(tmp_tabell, con=con, if_exists='replace', chunksize=10000, dtype=schema, index_label='id')  
+        # df = df[list(schema.keys())]
+        # df.to_sql(tmp_tabell, con=con, if_exists='replace', chunksize=10000, dtype=schema, index_label='id')  
+
+
+    if dtypes is not None:
+        df = df[list(dtypes.keys())]
+        sql_params['dtype']=dtypes
+
+    df.to_sql(**sql_params)
 
     # Ersätt befintlig tabell med den data som just laddades upp till "tmp_tabell"
     con.execute(f'DROP TABLE IF EXISTS {tabell}')
