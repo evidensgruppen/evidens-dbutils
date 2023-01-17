a45411e6fd3bf031cf7014d3bc4887f6c9dc8a59
Otto Jarhult <otto.jarhult@ad.evidensgruppen.se>
2023-01-17 10:12:13 +0100

    Uppdaterad version

--- dbutils/uppladdning.py
+++ dbutils/uppladdning.py
@@ -8,7 +8,7 @@ def ladda_upp(
     df      : pd.DataFrame, 
     tabell  : str, 
     databas : str,
-    schema  : Union[dict, None]=None
+    dtypes  : Union[dict, None]=None
 ) -> None:
     ''' Laddar upp df till vald databas. Uppladdning bygger i hög grad på pandas to_sql()-funktion. Vissa tillägg har
         gjorts för att passa våra behov, bl.a. laddas data inte direkt upp till tabellen "tabell" utan först till 
@@ -18,7 +18,7 @@ def ladda_upp(
             df      : Dataframe som ska laddas upp
             tabell  : Namn på tabell som ska genereras 
             databas : Namn på databas/schema
-            schema  : Datatyper för kolumnerna i df
+            dtypes  : Datatyper för kolumnerna i df
     '''
 
     # Anslut till databas
@@ -31,20 +31,30 @@ def ladda_upp(
     tmp_tabell = tabell + '_tmp'
     con.execute(f'DROP TABLE IF EXISTS {tmp_tabell}')
 
-    print(f'\nLaddar upp data till "{tmp_tabell}"')
+    # Parametrar för uppladdning
+    sql_params = {}
 
-    # Ladda upp data till ny tabell med suffix "_tmp"
-    if schema is None:
-        # Om inga datatyper specificerats väljs dessa automatiskt av pandas
-        df.to_sql(tmp_tabell, con=con, if_exists='replace', chunksize=10000, index_label='id')
+    sql_params['name']        = tmp_tabell
+    sql_params['con']         = con
+    sql_params['chunksize']   = 10000
+    sql_params['if_exists']   = 'replace'
+    sql_params['index_label'] = 'id'
 
-    else:
+    if dtypes is not None:
         # Om datatyper specificerats används keys för att se till att kolumnerna i df kommer i rätt ordning
-        df = df[list(schema.keys())]
-        df.to_sql(tmp_tabell, con=con, if_exists='replace', chunksize=10000, dtype=schema, index_label='id')  
+        df = df[list(dtypes.keys())]
+        sql_params['dtype']=dtypes
+
+    print(f'\nLaddar upp data till "{tmp_tabell}"')
+    df.to_sql(**sql_params)
 
     # Ersätt befintlig tabell med den data som just laddades upp till "tmp_tabell"
     con.execute(f'DROP TABLE IF EXISTS {tabell}')
     con.execute(f'ALTER TABLE {tmp_tabell} RENAME TO {tabell}')
     
     print(f'\nTabell "{tabell}" uppdaterad')
+
+
+
+
+
